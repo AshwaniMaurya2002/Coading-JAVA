7f29b09037e0ed6c75c3c8a4e8807d9bc6e69efc
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-03 22:08:42 +0530

    new

--- JAVA/practice/Variable and Method/variable.java
+++ JAVA/practice/Variable and Method/variable.java
@@ -1,19 +1,20 @@
-class variable{
-     int a=40;
-      void test(){
+class variable {
+    int a = 40;
+
+    void test() {
         // int a=60;
-         // local variable
-         starticMethod();
+        // local variable
+        starticMethod();
         System.out.println("Local variable a: " + a);
     }
 
-    static void starticMethod(){
+    static void starticMethod() {
         System.out.println("Static method called");
     }
 
-    static void display(){
+    static void display() {
         // static method
-        variable v=new variable();
+        variable v = new variable();
         System.out.println("Static method called");
         v.test();
         starticMethod();
@@ -22,4 +23,5 @@ class variable{
     public static void main(String[] args) {
         display();
     }
+
 }
