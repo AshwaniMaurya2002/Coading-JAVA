7b5af5279030e1bfe3dd78e271e6353c02fd0338
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-06 20:29:17 +0530

    Construtor

--- JAVA/SelfProgram/Static and Non Static/NonStaticBlock.java
+++ JAVA/SelfProgram/Static and Non Static/NonStaticBlock.java
@@ -1,7 +1,10 @@
 
 public class NonStaticBlock {
-    static  int count = 0;
-
+    static int count = 0;
+    // static is used because if it is non static then every time you create a
+    // object it initalize with 0
+    // and always give 1 but if you use static then then it assign to the same value
+    // and counter upated properly
     {
         System.out.println("i am the non static block");
 
@@ -11,6 +14,7 @@ public class NonStaticBlock {
 
         count++;
         System.out.println(count);
+        count=50;
     }
 
     public static void main(String[] args) {
---
+++ JAVA/SelfProgram/Static and Non Static/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+public static void main(String[] args) {
+        System.out.println("I am the main method");
+    }
