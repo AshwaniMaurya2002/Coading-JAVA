435b3db44a1ff7f7dfff8f0c676ba34a630d0019
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-14 10:22:40 +0530

    Pattern Update

---
+++ PatternFollowedApproach/ParatterWithSpace.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ParatterWithSpace {
+
+    public static void printPattern(int n) {
+        int patternWidth = 1;
+        int space = n - 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+
+            }
+            for (int j = 1; j <= patternWidth; j++) {
+                System.out.print("* ");
+            }
+            patternWidth++;
+            space--;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the no of rows");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printPattern(n);
+
+    }
+}
---
+++ PatternFollowedApproach/Pattern.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Pattern {
+
+    public static void printPattern(int n) {
+        int patternWidth = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= patternWidth; j++) {
+                System.out.print("* ");
+            }
+            patternWidth++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the no of rows");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printPattern(n);
+
+    }
+}
