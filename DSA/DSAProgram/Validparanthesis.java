5ec9f885944996c1c4b04e727cba0777f960ddc6
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-25 12:17:28 +0530

    ValidParanthesis

---
+++ DSA/DSAProgram/Validparanthesis.java
@@ -0,0 +1,42 @@
+
+import java.util.Stack;
+
+public class Validparanthesis {
+
+     static  Stack<Character> s=new Stack<>();
+
+    public static  boolean  valid(String str){
+
+        for (int i = 0; i <str.length(); i++) {
+
+            char c=str.charAt(i);
+
+           if (c == '(' || c == '{' || c == '[') {
+                s.push(c);
+            } 
+            else{
+                if(s.isEmpty()) {
+                    return false;
+
+                }
+
+                char top=s.pop();
+
+                if(c=='['&&top!=']'||c=='('&&top!=')'||c=='{'&&top!='}'){
+                    return false;
+                }
+                    
+            }
+        }
+        return s.isEmpty();
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        System.out.println(valid("{{(}"));
+
+    }
+    
+}
