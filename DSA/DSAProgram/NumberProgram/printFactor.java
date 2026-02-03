da3a711d1215db08ccc655437166d82f6d1b9310
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-03 19:18:03 +0530

    printFactor

---
+++ DSA/DSAProgram/NumberProgram/printFactor.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class printFactor {
+
+    public static void printFactorAll(int n){
+        
+int count=0;
+        for (int i = 1; i*i < n; i++) {
+            if(n%i==0){
+                int fact1=i;
+                int fact2=n/i;
+                if(fact1 !=fact2){
+                    System.out.print(fact1 +" "+ fact2);
+                    count+=2;
+                }
+
+                else{
+                    System.out.print(fact1+" ");
+                    count++;
+                }
+            }
+
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the Number Of For the Factors");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printFactorAll(n);
+    }
+    
+}
