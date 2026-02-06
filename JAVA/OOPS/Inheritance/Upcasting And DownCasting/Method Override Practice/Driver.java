04df214b2c1dac14aeaac0fd2ecd8fa8173fa492
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-06 20:35:40 +0530

    Override driver

---
+++ JAVA/OOPS/Inheritance/Upcasting And DownCasting/Method Override Practice/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+
+    public static void main(String[] args) {
+        
+
+        Student a=new Ashwani();
+        // Ashwani as=(Ashwani)a;
+        System.out.println(((Ashwani)a).getName());
+    }
+    
+}
