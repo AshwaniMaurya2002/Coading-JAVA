cb3357d8d3248e89d744455ecbbc22a2cbfb23bf
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-08 18:42:35 +0530

    .

--- Practice/upcasting/downcasrting/Driver.java
+++ Practice/upcasting/downcasrting/Driver.java
@@ -4,6 +4,9 @@ public class Driver {
     public static void main(String[] args) {
         
         Student ash=new Student();
+
+        //if methods are non static then method runs as object created
+        //else it does not override and parent reffernce will run
         
         System.out.println(ash instanceof Ashwani);
 
