0d5b1c75b62bf91af9ca7344286083cb5362d0cc
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-10 20:43:59 +0530

    exception

---
+++ JAVA/ExceptionHandling/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+
+
+
+    public static void main(String[] args) {
+        
+
+        try {
+
+            int i=10/0;
+
+            int arr[]={1,2,3};
+            System.out.println(arr[5]);
+
+
+
+            
+        } catch (ArithmeticException e) {
+            System.out.println("not divisible");
+
+            
+        }
+
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("out of memorya");
+
+        }
+
+
+    }
+    
+}
