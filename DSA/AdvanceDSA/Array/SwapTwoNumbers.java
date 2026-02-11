c510c290be9d914850e8f4d7b9a879dd9a964988
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-11 20:27:13 +0530

    swapTwoValue

---
+++ DSA/AdvanceDSA/Array/SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class SwapTwoNumbers {
+
+    public static void swap(int[] p,int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+
+    public static void main(String[] args) { 
+        int[] a={10,20,30,40,50};
+        int i=1;
+        int j=3;
+        swap(a, i, j);
+        System.out.println(Arrays.toString(a));
+    }
+    
+}
