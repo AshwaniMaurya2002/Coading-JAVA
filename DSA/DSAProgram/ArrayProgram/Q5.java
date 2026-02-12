6c6074376fc59059044957dcd4eac31113736a95
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-12 20:02:46 +0530

    update Q5

---
+++ DSA/DSAProgram/ArrayProgram/Q5.java
@@ -0,0 +1,17 @@
+class Q5{
+
+    public static void getEvenIndex(int[] a){
+        for (int i = a.length-1; i >=0; i--) {
+           System.out.println(a[i]);
+           
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int[] a={10,20,30,40,50};
+
+        getEvenIndex(a);
+    }
+
+}
