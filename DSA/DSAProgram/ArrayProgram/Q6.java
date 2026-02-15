488221796dec869b138a01599d2f96ed5a892140
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-15 17:21:29 +0530

    .

---
+++ DSA/DSAProgram/ArrayProgram/Q6.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class Q6 {
+
+
+    public  static  void getSum(int[] a){
+        int sum=0;
+        for(int s:a){
+            sum+=s;
+        }
+
+        System.out.println(sum);
+
+        for(int i=0;i<a.length;i++){
+            a[i]=sum-a[i];
+        }
+
+        System.out.println("The new Array is"+ Arrays.toString(a));
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] a={1,3,2,4,5,2};
+
+        getSum(a);
+        
+    }
+    
+}
