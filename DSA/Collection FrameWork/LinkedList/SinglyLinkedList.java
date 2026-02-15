3256e8e308f95b137ee3a15b01b688587442eff6
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-15 16:09:35 +0530

    .

--- DSA/Collection FrameWork/LinkedList/SinglyLinkedList.java
+++ DSA/Collection FrameWork/LinkedList/SinglyLinkedList.java
@@ -10,14 +10,15 @@ public class SinglyLinkedList {
         public Node() {
         }
 
-        public Node(int data, Node next) {
+        public Node(int data) {
             this.data = data;
-            this.next = null;
         }
+      
+    }
+
+    public static void main(String[] args) {
         
+      
 
-
-
-        
     }
 }
