1920b6d95e20fdfaa4675075bb7d892eaafff130
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-20 20:18:00 +0530

    .

--- DSA/DSAProgram/ArrayProgram/frequency/ArrayFreq.java
+++ DSA/DSAProgram/ArrayProgram/frequency/ArrayFreq.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class ArrayFreq {
 
     public static  void frequency(int[] a){
+        int n=a.length;
         int max=a[0];
         int min=a[0];
 
@@ -11,17 +14,32 @@ public class ArrayFreq {
                 min=i;
         }
 
+        /*
+       ? if i is given
+       * index= i+min;
+
+       ?if value is given
+       * index=value-min;
+
+        */
+
+
         int[] freq=new int[max-min+1];
 
-        for (int i = 0; i <a.length; i++) {
-            
+       
+        for (int i : freq) {
+            freq[i-min]++;
         }
+
+        System.out.println(Arrays.toString(freq));
 
     }
 
 
     public static void main(String[] args) {
         
+int [] a={12,1,2,12,12,53,43,53,34};
+frequency(a);
 
 
     }
