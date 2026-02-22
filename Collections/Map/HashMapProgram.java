68d26b7026ca9802f88c2aa7a7d0bfad634bf037
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-22 20:06:45 +0530

    update

---
+++ Collections/Map/HashMapProgram.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapProgram {
+
+    public static void main(String[] args) {
+        
+        Map<Integer,Integer>m=new HashMap();
+        m.put(14, 14);
+        m.put(15, 15);
+        m.put(12, 12);
+        m.put(13, 13);
+        System.out.println(m);
+        
+    }
+    
+}
