281b6979a0484326b85d877b15ac9abffc5a0f92
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-22 20:09:48 +0530

    .

---
+++ Collections/Map/LinkedHashMap.java
@@ -0,0 +1,11 @@
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class LinkedHashMap {
+    public static void main(String[] args) {
+        
+        Map<Integer,Integer> m = new LinkedHashMap<>();
+        System.out.println(m);
+    }
+}
