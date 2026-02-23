92b7b1264c625e2f4af9bc1858308606a267ce7e
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-23 18:51:23 +0530

    selectionSort

--- DSA/Sorting/SelectoinSort.java
+++ DSA/Sorting/SelectoinSort.java
@@ -1,3 +1,16 @@
 public class SelectoinSort {
     
+    public static void Selection(int[] a){
+
+        int n=a.length;
+        for (int i = 0; i <n-1; i++) {
+            for (int j = 0; j < n; j++) {
+
+                
+            }
+            
+        }
+
+    }
+
 }
