a45b5b108a4d2d48b9601b1ce54393a2bd2e40ba
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-23 18:58:22 +0530

    selectionSort UPdate program

--- DSA/Sorting/SelectoinSort.java
+++ DSA/Sorting/SelectoinSort.java
@@ -1,15 +1,38 @@
+import java.util.Arrays;
+
 public class SelectoinSort {
     
     public static void Selection(int[] a){
 
         int n=a.length;
         for (int i = 0; i <n-1; i++) {
-            for (int j = 0; j < n; j++) {
+            int minIndex=i, minValue=a[i];
+            for (int j = i+1; j < n; j++) {
+                if(a[i]<minValue){
+                    minIndex=j;
+                    minValue=a[j];
+                }
+
 
                 
             }
+
+            // this is used For swapping;
+            a[minIndex]=a[i];
+            a[i]=minValue;
             
         }
+    }
+
+
+    public static void main(String[] args) {
+        
+        int[] a={12,1,3,23,52,7,9,0};
+        System.out.println(Arrays.toString(a));
+        Selection(a);
+        System.out.println(Arrays.toString(a));
+
+
 
     }
 
