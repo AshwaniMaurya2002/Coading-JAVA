a8d6d93d611bb017135a4bb89bb7f77be8d9da4b
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-25 19:02:31 +0530

    updated the final

--- DSA/DSAProgram/ArrayProgram/Questions/SecondMax.java
+++ DSA/DSAProgram/ArrayProgram/Questions/SecondMax.java
@@ -1,9 +1,26 @@
 public class SecondMax {
 
 
-    public static void secondMax(int[] a)
+    public static int secondMax(int[] a){
+        long max=Long.MIN_VALUE;
+        long secondMax=Long.MIN_VALUE;
+
+        for (int i : a) {
+            if(i>max){
+                secondMax=max;
+                max=i;
+            }else if(i<max&&i!=secondMax){
+                secondMax=i;
+            }
+        }
+        return secondMax!=Long.MIN_VALUE?(int)secondMax:(int)max;
+    }
 
     public static void main(String[] args) {
+
+        int[] a={12,2,4,24,2,5,63,63,637,231,425,1000,1004,10002};
+       System.out.println( secondMax(a));
+        
         
     }
     
