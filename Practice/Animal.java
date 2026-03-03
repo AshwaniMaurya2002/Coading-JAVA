89bef9eb2e1818b1dc3c0c7b039411a77757162b
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-03 11:51:01 +0530

    start

--- Practice/Animal.java
+++ Practice/Animal.java
@@ -1,6 +1,9 @@
 public class Animal {
     public static void drink(){
-        System.out.println("animal class");
+        
+        String a=new String("sjhgs");
+        System.out.println(a);
+
     }
 
 }
