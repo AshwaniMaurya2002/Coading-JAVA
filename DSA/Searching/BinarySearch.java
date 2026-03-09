c1b23f35337774c8b86020ec02ad65512356d4e4
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-09 18:42:22 +0530

    made parameters

--- DSA/Searching/BinarySearch.java
+++ DSA/Searching/BinarySearch.java
@@ -1,7 +1,9 @@
 public class BinarySearch {
 
 
-    public static int binarySearch
+    public static int binarySearch(int []a,int target,int start,int end){
+        m
+    }
     public static void main(String[] args) {
         
     }
