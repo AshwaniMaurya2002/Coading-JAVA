326d62a9955f8806f685844130cd87ca57604962
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-09 18:44:17 +0530

    made decision elemets

--- DSA/Searching/BinarySearch.java
+++ DSA/Searching/BinarySearch.java
@@ -2,7 +2,12 @@ public class BinarySearch {
 
 
     public static int binarySearch(int []a,int target,int start,int end){
-        m
+        if(start>end)
+            return -1;
+        int mid=start+(end-start)/2;
+        if(mid==target)
+            return mid;
+        else if
     }
     public static void main(String[] args) {
         
