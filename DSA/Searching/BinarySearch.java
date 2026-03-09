36091fbd188d9ff7d89c78621f275a4bad45f544
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-09 18:50:36 +0530

    completed the binarSearch Program

--- DSA/Searching/BinarySearch.java
+++ DSA/Searching/BinarySearch.java
@@ -5,12 +5,18 @@ public class BinarySearch {
         if(start>end)
             return -1;
         int mid=start+(end-start)/2;
-        if(mid==target)
+        if(a[mid]==target)
             return mid;
-        else if
+        else if(a[mid]>target)
+            return binarySearch(a, target, start, mid-1);
+        else
+            return  binarySearch(a, target, mid+1, end);
     }
+    
     public static void main(String[] args) {
-        
+
+        int[] a={1,2,5,6,8,44,356,674,999};
+    System.out.println(  binarySearch(a, 44, 0, a.length-1));        
     }
     
 }
