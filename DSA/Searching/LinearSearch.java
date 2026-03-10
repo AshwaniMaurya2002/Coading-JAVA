478db4b758ce82fc35fa0c214a59e43e0f8487b0
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-10 19:24:35 +0530

    printLn

--- DSA/Searching/LinearSearch.java
+++ DSA/Searching/LinearSearch.java
@@ -1,5 +1,21 @@
 class LinearSearch{
 
-    public  static int linearSearch(int[]a ,int target)
+    public  static int linearSearch(int[]a ,int target){
+        for(int i=0;i<a.length;i++){
+            if(a[i]==target)
+                return  i;
+
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        
+        int[] a={1,80,42,5,6,3,67,8,8,346,67,7,4,654,7,754,4,3646,634};
+
+        System.out.println(linearSearch(a, 66  ));
+
+    }
 
 }
