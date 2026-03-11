f01402dff17b378b9d3275be588e9f0c455d1f9d
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-11 23:16:01 +0530

    builderPattern

--- Builder Pattern/Employ.java
+++ Builder Pattern/Employ.java
@@ -3,7 +3,10 @@ public class Employ {
     String name;
     int id;
 
-    
+    int age;
+    String address;
+
+
 
     public Employ() {
     }
@@ -13,7 +16,51 @@ public class Employ {
         this.id = id;
     }
 
-    
+    public Employ(Builder builder) {
+        this.name = builder.name;
+        this.id = builder.id;
+        this.age = builder.age;
+        this.address = builder.address;
+    }
 
-    
+    class Builder{
+
+    String name;
+    int id;
+
+    int age;
+    String address;
+
+        public Builder() {
+        }
+
+        public Builder(String name, int id, int age, String address) {
+            this.name = name;
+            this.id = id;
+            this.age = age;
+            this.address = address;
+        }
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+        public Employ build(){
+            return new Employ(this);
+        }
+    }
 }
--- Builder Pattern/EmployDriver.java
+++ Builder Pattern/EmployDriver.java
@@ -1,4 +1,13 @@
 public class EmployDriver {
     
-    
+    public static void main(String[] args) {
+        
+        Employ employ = new Employ().new Builder().setAddress("noida").setAge(54).build();
+
+        System.out.println("Employ Name: " + employ.name);
+        System.out.println("Employ ID: " + employ.id);
+        System.out.println("Employ Age: " + employ.age);
+        System.out.println("Employ Address: " + employ.address);
+
+    }
 }
