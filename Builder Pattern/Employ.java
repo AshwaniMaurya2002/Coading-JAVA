822512dd65fa754c59c337da6ecc175c40e81322
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-11 23:00:59 +0530

    BuilderPattern

---
+++ Builder Pattern/Employ.java
@@ -0,0 +1,19 @@
+public class Employ {
+    //mandatory
+    String name;
+    int id;
+
+    
+
+    public Employ() {
+    }
+
+    public Employ(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    
+
+    
+}
