3ed3a00402dfe2c1e44a846488f608db882709fd
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-12 21:14:46 +0530

    made changes in exception

--- JAVA/ExceptionHandling/StudentDriver.java
+++ JAVA/ExceptionHandling/StudentDriver.java
@@ -1,11 +1,25 @@
+
+import java.util.Scanner;
+
+
+
 public class StudentDriver {
     
     public static void main(String[] args) {
         
-        Student student = new Student(1, null);
-        Student student1 = new Student(2, "John");
+        // Student student = new Student(1, null);
+        // Student student1 = new Student(2, "John");
 
-        System.out.println(student.name);
-        
+        // System.out.println(student.name);
+
+        // String s2=null;
+
+        // System.out.println(s2.charAt(0));
+
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        byte b = sc.nextByte();
+
+        System.out.println("You entered: " + b);
     }
 }
