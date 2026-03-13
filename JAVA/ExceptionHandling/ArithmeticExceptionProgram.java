be19b403335298bc5f5702470fc02c8d31f96267
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-13 09:15:15 +0530

    exception practice

---
+++ JAVA/ExceptionHandling/ArithmeticExceptionProgram.java
@@ -0,0 +1,14 @@
+public class ArithmeticExceptionProgram {
+
+
+    public static void main(String[] args) {
+        
+        try {
+             System.out.println(12/0);
+        } catch (Exception e) {
+        }
+       
+
+    }
+    
+}
