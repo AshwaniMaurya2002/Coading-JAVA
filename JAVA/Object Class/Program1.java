57176f810fc9e5f8c507e4a1cba8f24b855d6bbc
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-14 19:22:55 +0530

    making Equals method

--- JAVA/Object Class/Program1.java
+++ JAVA/Object Class/Program1.java
@@ -16,11 +16,17 @@ public class Program1 {
 
     
 @Override
-    public String equal(Object obj){
+    public boolean  equal(Object obj){
 
         if(this==obj)
             return true;
 
+        else if(obj==null || this.getClass()!=obj.getClass())
+            return  false;
+
+        else
+            
+            return  
 
     }      
     
@@ -28,6 +34,9 @@ public class Program1 {
 
     public static void main(String[] args) {
 
+        Program1 p=new Program1();
+        Program1 p1=new Program1();
+        
 
 
         }
