5405312b8fc8de287ae1912560a0b17a453543cb
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-19 23:23:14 +0530

    commit

--- DSA/DSAProgram/ArrayProgram/Questions/Q19.java
+++ DSA/DSAProgram/ArrayProgram/Questions/Q19.java
@@ -2,12 +2,26 @@ public class Q19 {
 
 
 
-    public static void checkPalindrome(int[] a){
-        
+    public static boolean  checkPalindrome(int[] a,int[] b){
+
+        int indx1=0;
+        int indx2=b.length-1;
+        if(a.length!=b.length)
+            return false;
+
+        while(a.length-1){
+            if(a[indx1++]!=b[indx2--])
+                return false;
+            
+        }  
+
     }
     
 
     public static void main(String[] args) {
+
+        int[] a={10 ,20 ,30, 40, 80, 70 ,25};
+        int[] b={25, 70, 80, 40 ,30 ,20 ,10};
         
     }
 }
