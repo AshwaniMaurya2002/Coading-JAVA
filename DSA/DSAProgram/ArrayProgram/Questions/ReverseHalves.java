a77bf2c02c9ff53a46722ccb608658dd06908c60
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-19 23:49:57 +0530

    completed the code for half array roatate

---
+++ DSA/DSAProgram/ArrayProgram/Questions/ReverseHalves.java
@@ -0,0 +1,25 @@
+public class ReverseHalves {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60, 70};
+        int n = arr.length;
+
+        // Reverse first half
+        for (int i = 0, j = n/2; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        // Reverse second half
+        for (int i = (n/2) + 1, j = n - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        // Print result
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
