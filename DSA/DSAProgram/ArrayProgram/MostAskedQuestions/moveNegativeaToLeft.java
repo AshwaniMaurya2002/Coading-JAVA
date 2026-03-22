d891d069be5d50900f7d0ac5db11964428f571bd
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-22 13:45:03 +0530

    done with the program

--- DSA/DSAProgram/ArrayProgram/MostAskedQuestions/moveNegativeaToLeft.java
+++ DSA/DSAProgram/ArrayProgram/MostAskedQuestions/moveNegativeaToLeft.java
@@ -5,7 +5,12 @@ public class moveNegativeaToLeft {
     public static void move(int[] a){
         int j=0;
         for(int i=0;i<a.length;i++){
-
+            if(a[i]<0){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                j++;
+            }
         }
     }
 
