98ff7f94a08a1dd748adad454659823e3c11a79d
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-22 13:39:59 +0530

    pushed

--- DSA/DSAProgram/ArrayProgram/MostAskedQuestions/moveNegativeaToLeft.java
+++ DSA/DSAProgram/ArrayProgram/MostAskedQuestions/moveNegativeaToLeft.java
@@ -1,16 +1,19 @@
-public class moveNegativeaToLeft {
+import java.util.Arrays;
 
+public class moveNegativeaToLeft {
 
     public static void move(int[] a){
 
 
-        
+
 
     }
 
 
     public static void main(String[] args) {
-        
+        int[] a={12,-12,23,2,4,-1,-53,5};
+        move(a);
+        System.out.println(Arrays.toString(a));
 
     }
     
