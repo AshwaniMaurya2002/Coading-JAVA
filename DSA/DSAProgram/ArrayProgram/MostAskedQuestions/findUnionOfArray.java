393c4c590e7e1ba104b528f9808229a01df490a6
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-23 20:57:54 +0530

    made the program

--- DSA/DSAProgram/ArrayProgram/MostAskedQuestions/findUnionOfArray.java
+++ DSA/DSAProgram/ArrayProgram/MostAskedQuestions/findUnionOfArray.java
@@ -1,8 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class findUnionOfArray {
     
-    public static void unionOfArray(int[]a,int[] b){
+    public static int[] unionOfArray(int[]a,int[] b){
 
-        Set<Integer> union=
+        Set<Integer> union=new HashSet<>();
+
+        for (Integer integer : a) {
+            union.add(integer);
+            
+        }
+        for (Integer integer : b) {
+            union.add(integer);
+            
+        }
+
+       
+        
+        int[] result=new int[union.size()];
+        int i=0;
+        for (Integer integer : union) {
+            result[i++]=integer;
+        }
+        return result;
 
 
     }
