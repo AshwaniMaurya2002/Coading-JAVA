b464d838f62385ee47852ecc22bb500d750376a2
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-25 20:24:48 +0530

    union

--- DSA/DSAProgram/ArrayProgram/MostAskedQuestions/findUnionOfArray.java
+++ DSA/DSAProgram/ArrayProgram/MostAskedQuestions/findUnionOfArray.java
@@ -22,8 +22,6 @@ public class findUnionOfArray {
             result[i++]=integer;
         }
         return result;
-
-
     }
     public static void main(String[] args) {
         int[] a={1,2,3,1,3,4,5};
