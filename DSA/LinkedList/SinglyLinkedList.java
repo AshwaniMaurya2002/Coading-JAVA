a90dacdb6f8dbc24e8046c396c560c6128533a50
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-26 23:18:06 +0530

    maded linkedlist class

--- DSA/LinkedList/SinglyLinkedList.java
+++ DSA/LinkedList/SinglyLinkedList.java
@@ -1,6 +1,16 @@
 public class SinglyLinkedList {
 
-    class Node 
+
+   private  class Node {
+        private  int val;
+        private  Node next;
+
+         Node(int val) {
+            this.val=val;
+            Node next=null;
+        }
+
+    }
     
 
 }
