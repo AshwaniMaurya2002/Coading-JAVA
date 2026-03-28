398b207832db9e971f73bc42f6179b3f760523f1
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-28 20:32:51 +0530

    made eager instantiation

--- JAVA/PrivateConstructor/Eager Instantiation/Browser.java
+++ JAVA/PrivateConstructor/Eager Instantiation/Browser.java
@@ -1,5 +1,19 @@
 public class Browser {
 
     private static Browser b=null;
+
+    public Browser() {
+    }
+
+    public static Browser getInstance(){
+        if(b==null)
+            b=new Browser();
+        return b;
+    }
+
+    public void search(){
+        System.out.println("searching the item");
+        
+    }
     
 }
