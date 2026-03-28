19caf82aec7a030186e999230c30c94b280eef9e
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-28 20:47:57 +0530

    Immmutable class

---
+++ JAVA/Immutable class/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+
+
+    Vehicle v =new Vehicle("TATA",120);
+    v.getBrand();
+}
---
+++ JAVA/Immutable class/Vehicle.java
@@ -0,0 +1,20 @@
+public final class Vehicle {
+
+    private  final String brand;
+    private final  int hp;
+
+    public Vehicle(String brand, int hp) {
+        this.brand = brand;
+        this.hp = hp;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+        
+}
