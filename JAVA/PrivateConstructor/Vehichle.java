ba18f11f62d985d265ae6c29bf5cc1adc676ac65
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-28 20:04:05 +0530

    update

--- JAVA/PrivateConstructor/Vehichle.java
+++ JAVA/PrivateConstructor/Vehichle.java
@@ -3,5 +3,14 @@ public class Vehichle {
 
     String brand;
     int price;
+
+    private  Vehichle(String brand, int price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public void drive(){
+        System.out.println("drive");
+    }
     
 }
