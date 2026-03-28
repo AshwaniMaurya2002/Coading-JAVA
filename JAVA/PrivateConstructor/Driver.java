1a11292c51454940a5c6f0cf2913fd712b4869c1
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-28 20:22:42 +0530

    made completely

--- JAVA/PrivateConstructor/Driver.java
+++ JAVA/PrivateConstructor/Driver.java
@@ -2,7 +2,14 @@ public class Driver {
 
 
     public static void main(String[] args) {
-    Vehichle v=new Vehicle();
+    // Vehichle v=new Vehicle("string",2134);  //not called because constructor is private
+
+    //! we make a static method 
+
+   Vehichle v= Vehichle.getInstance();
+   v.drive();
+   v.test();
+
     }
 
     
--- JAVA/PrivateConstructor/Vehichle.java
+++ JAVA/PrivateConstructor/Vehichle.java
@@ -1,16 +1,22 @@
 public class Vehichle {
 
 
-    String brand;
-    int price;
+    private static  String brand;
+
+    private static  int price;
+
+    private static  Vehichle v=null;
+
 
     private  Vehichle(String brand, int price) {
         this.brand = brand;
         this.price = price;
     }
 
-    public Vehichle getInstance(){
-        
+    public static Vehichle getInstance(){
+        if(v==null)
+             v= new Vehichle(brand, price);
+        return v;
     }
 
     public void drive(){
