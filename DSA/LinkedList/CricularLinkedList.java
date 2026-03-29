1a3afb01e28e5d91b2492165b8b72f47f513560f
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-29 21:45:24 +0530

    cicularLinked list

---
+++ DSA/LinkedList/CricularLinkedList.java
@@ -0,0 +1,22 @@
+public class CricularLinkedList {
+
+    
+
+    class Node{
+
+        int val;
+        Node next;
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
