e7fce9b172309e819c52b44b73587f6bd0141c17
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-29 21:49:32 +0530

    made methods

--- DSA/LinkedList/CricularLinkedList.java
+++ DSA/LinkedList/CricularLinkedList.java
@@ -1,5 +1,7 @@
 public class CricularLinkedList {
 
+
+    Node head;
     
 
     class Node{
@@ -15,7 +17,31 @@ public class CricularLinkedList {
         
     }
 
+    public void add(int val){
+        Node newNode=new Node(val, head);
+        if(head==null)
+            head=newNode;
+        else{
+            Node temp=head;
+            while(temp!=null){
+                temp=temp.next;
+
+            }
+            temp.next=newNode;
+
+        }
+
+    
+    }
+
+    
+
+
+
     public static void main(String[] args) {
+
+        CricularLinkedList cl=new CricularLinkedList();
+        cl.add(12);
         
     }
     
