b53b3a9bd410d49c6156c52861d3b84aa00ef2fb
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-30 22:31:08 +0530

    pushed nested method

---
+++ JAVA/NestedClass/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        
+        NestedClass n=new NestedClass();
+        n.rollNo=242;
+        n.id=202;
+
+        NestedClass.Car nc=NestedClass.new Car();
+
+    }
+    
+}
---
+++ JAVA/NestedClass/NestedClass.java
@@ -0,0 +1,24 @@
+public class NestedClass {
+
+
+    String name;
+    int id;
+    int rollNo;
+
+
+    class Car{
+        String brand;
+        int hp;
+
+
+      
+
+    }
+
+
+    class Tyre{
+        int noOfTyre;
+        
+    }
+    
+}
