5ef3ccf58132c22be360d1f268ad73881ab80630
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-02 20:19:34 +0530

    updated the exsisting code

--- JAVA/OOPS/Polymorphism/Method OverLoading/Maths.java
+++ JAVA/OOPS/Polymorphism/Method OverLoading/Maths.java
@@ -4,15 +4,12 @@ public class Maths {
     }
     //change sequence 
     public static void add(double x,int y){
-
         System.out.println(x+y);
     }
 
     public static void add(int x,double  y){
         System.out.println(x+y);
     }
-
-
     //change no of agruments
     public static void add(int x,int y,int z){
         System.out.println(x+y+z);
