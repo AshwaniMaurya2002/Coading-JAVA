8a992c6e5a320c2f4299fc1d6718b9f91fe94b7e
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-27 22:12:08 +0530

    done for the day

--- JAVA/Lambda Expression/Driver.java
+++ JAVA/Lambda Expression/Driver.java
@@ -3,7 +3,9 @@ public class Driver {
     public static void main(String[] args) {
         
         Lambda add = (a, b) -> a + b;
+        Lambda mul=(a,b)->a*b;
 
         System.out.println(add.get(10, 20)); // Output: 30
+        System.out.println(mul.get(20, 30));
     }
 }
