cc4110c6e2cad422aabbbd01e8848ade9053a098
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-30 18:41:26 +0530

    ,.

--- JAVA/Programs/Assigment/demo.java
+++ JAVA/Programs/Assigment/demo.java
@@ -1,17 +1,16 @@
 public class demo {
     public static void main(String[] args) {
 
-        int ans=fact(23);
+        int ans = fact(23);
 
         System.out.println(ans);
-
     }
 
-    public static int fact(int number){
-        if(number==0||number==1){
+    public static int fact(int number) {
+        if (number == 0 || number == 1) {
             return 1;
 
-        }
-        else return number*(fact(number-1));
+        } else
+            return number * (fact(number - 1));
     }
 }
