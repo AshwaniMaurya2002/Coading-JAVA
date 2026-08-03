9316072c5c03c8f7aa11a569b6d8b427a251c54d
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-03 14:09:55 +0530

    hashamap

---
+++ CoreJavaRevision/Collection/Que3.java
@@ -0,0 +1,15 @@
+
+import java.util.HashMap;
+
+public class Que3 {
+
+                public static void main(String[] args) {
+                                HashMap<Integer, String> h = new HashMap<>();
+
+                                h.put(101, "ashwani");
+                                h.put(102, "mohan");
+                                h.put(103, "rohan");
+
+                                System.out.println(h);
+                }
+}
