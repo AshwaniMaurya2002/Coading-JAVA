9929510c73a8e10176b0a5450ea3147c4e924864
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-07 13:18:28 +0530

    done with getname and getPriority

--- CoreJavaRevision/Multithreaading/First.java
+++ CoreJavaRevision/Multithreaading/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
     }
 }
