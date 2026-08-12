d8a2f89527463d69904c9ea33a5a92d07dd34421
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-12 11:15:11 +0530

    doing practice on arraylist

---
+++ CoreJavaRevision/Collection Question Practice/ArrayList1.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayList1 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList();
+
+        list.add(12);
+        list.add(53);
+        list.add(532);
+        list.add(55);
+        list.add(23);
+//         Iterator<Integer> it = list.iterator();
+
+//         while (it.hasNext()) {
+//             System.out.println(it.next());
+//         }
+//         System.out.println(Collections.max(list));
+
+        int sum = 0;
+        int count = 0;
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            Integer elem = it.next();
+            sum += elem;
+            count++;
+
+        }
+        System.out.println(sum);
+        System.out.println(sum / count);
+
+    }
+
+}
