232c4caf997411f9ee2f497f1e53dd7643cf9ee8
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-13 00:05:42 +0530

    completed the streamAPI methods

--- CoreJavaRevision/java8/strameAPi/streamapi.java
+++ CoreJavaRevision/java8/strameAPi/streamapi.java
@@ -9,6 +9,13 @@ public class streamapi {
         List<Integer> numbers = Arrays.asList(10, 25, 30, 45, 50, 65);
 
 //         numbers.stream().filter(n -> n > 40).forEach(System.out::println);
-        numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::print);
+//         numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::print);
+//         List<Integer> num = numbers.stream().map(n -> n * 10).collect(Collectors.toList());
+//         System.out.println(num);
+//         numbers.stream().sorted(Collections.reverseOrder()).forEach(System.out::println);
+        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+
+        System.out.println(sum);
+
     }
 }
