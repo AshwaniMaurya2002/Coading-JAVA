f96fad13323bdee782336033b9baf109b205bf87
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-20 01:07:13 +0530

    update  h2 server

--- SpringBoot/demo/src/main/java/com/example/vscode/myapp/demo/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.vscode.myapp.demo.Entity.Employee;
-
-public interface EmployeeRepository
-        extends JpaRepository<Employee, Long> {
-}
