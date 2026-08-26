2d4672cab7be3679e59ddba9547a782bf7b4085b
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-08-26 23:30:36 +0530

    pushed

--- CoreJavaRevision/Collection/Map/HashMap1.java
+++ CoreJavaRevision/Collection/Map/HashMap1.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 class HashMap1 {
                 public static void main(String[] args) {
 
+                    
                             HashMap<Integer, String> h = new HashMap<>();
 
                                 h.put(1, "ashwani");
