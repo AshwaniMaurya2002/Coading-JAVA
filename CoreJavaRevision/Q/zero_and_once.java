69768d3f11913374f6c4e18ecb7a0339aa358b13
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-09-04 14:07:09 +0530

    complete

--- CoreJavaRevision/Q/zero_and_once.java
+++ CoreJavaRevision/Q/zero_and_once.java
@@ -1,14 +1,39 @@
+
+import java.util.Arrays;
+
 public class zero_and_once {
 
+    public static int[] getSorted(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
 
-                public static void getSorted(int[] arr){
-                                
-                }
+        while (i < j) {
+            if (arr[i] == 0) {
+                i++;
+            } else if (arr[j] != 0) {
+                j--;
+            } else {
 
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
 
+                i++;
+                j--;
 
-                public static void main(String[] args) {
-                                
-                }
-                
+            }
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0};
+
+        int[] ans = getSorted(arr);
+
+        System.out.println(Arrays.toString(ans));
+
+    }
+
 }
